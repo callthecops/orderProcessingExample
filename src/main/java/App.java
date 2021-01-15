b4675e1af338f283d13f8bde1c6ebf2341b26f62
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-15 20:55:01 +0200

    utils

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,44 +1,57 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import Utils.Utils;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
+        Utils utils = new Utils();
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse("src/main/resources/input/Order01.xml");
-            NodeList orderList = document.getElementsByTagName("order");
-            for (int i = 0; i < orderList.getLength(); i++) {
-                Node node = orderList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element order = (Element) node;
-                    String date = order.getAttribute("created");
-                    String id = order.getAttribute("ID");
-                    System.out.println(date);
-                    System.out.println(id);
-                    NodeList nodeList = order.getChildNodes();
-                    for (int j = 0; j < nodeList.getLength(); j++) {
-                        Node child = nodeList.item(j);
-                        if (child.getNodeType() == Node.ELEMENT_NODE) {
-                            Element product = (Element) child;
-                            System.out.println("Product" + id + ":" + product.getTagName() + "is" + product.getTextContent());
-                            String description = product.get
+            NodeList orderNodes = utils.selectTagNodes("src/main/resources/input/Order01.xml", "order", factory);
+            System.out.println(orderNodes.getLength());
 
-                        }
-                    }
-
-                }
-            }
-        } catch (ParserConfigurationException | SAXException | IOException exception) {
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
+
     }
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        try {
+//            DocumentBuilder builder = factory.newDocumentBuilder();
+//            Document document = builder.parse("src/main/resources/input/Order01.xml");
+//            NodeList orderList = document.getElementsByTagName("order");
+//            for (int i = 0; i < orderList.getLength(); i++) {
+//                Node node = orderList.item(i);
+//                if (node.getNodeType() == Node.ELEMENT_NODE) {
+//                    Element order = (Element) node;
+//                    String date = order.getAttribute("created");
+//                    String id = order.getAttribute("ID");
+//                    System.out.println(date);
+//                    System.out.println(id);
+//                    NodeList nodeList = order.getChildNodes();
+//                    for (int j = 0; j < nodeList.getLength(); j++) {
+//                        Node child = nodeList.item(j);
+//                        if (child.getNodeType() == Node.ELEMENT_NODE) {
+//                            Element product = (Element) child;
+//                            System.out.println(product.getTagName());
+//                            NodeList productChildren = product.getChildNodes();
+//                            for(int k = 0;k<productChildren.getLength();k++){
+//                                Node productChild = productChildren.item(i);
+//                                System.out.println(productChild.getTextContent());
+//                            }
+////                            System.out.println("Product" + id + ":" + product.getTagName() + "is" + product.getTextContent());
+//
+//
+//                        }
+//                    }
+//
+//                }
+//            }
+//        } catch (ParserConfigurationException | SAXException | IOException exception) {
+//            exception.printStackTrace();
+//        }
+//    }
 }
