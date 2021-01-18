24d681a3d58aa32bbe221e1ac4116e76f422d530
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-18 15:25:09 +0200

    guava

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import Utils.Utils;
+import com.google.common.collect.Multimap;
 import model.Order;
 import model.Orders;
 import model.Product;
@@ -10,33 +11,12 @@ public class App {
     public static void main(String[] args) {
         Utils utils = new Utils();
         try {
+
             Orders orders = utils.unMarshal();
             List<Order> order = orders.getOrder();
             List<Product> products = utils.extractProducts(order);
-            for (Product p : products) {
-                System.out.println(p.getDescription());
-                System.out.println(p.getSupplier());
-                System.out.println(p.getGtin());
-                System.out.println(p.getPrice());
-                System.out.println("ORDER ID");
-                System.out.println(p.getOrderId());
-            }
-
-//            Order order1 = order.get(0);
-//            Order order2 = order.get(1);
-//            List<Product> products = order1.getProducts();
-//            List<Product> products1 = order2.getProducts();
-//            System.out.println(products.size());
-//            System.out.println(products1.size());
-//            for (Product p : products) {
-//                System.out.println(p.getDescription());
-//            }
-//            System.out.println("BLANK");
-//            System.out.println("BLANK");
-//            System.out.println("BLANK");
-//            for (Product p : products1) {
-//                System.out.println(p.getDescription());
-//            }
+            Multimap<String, Product> productsByVendors = utils.selectProductsByVendors(products);
+            utils.marshall(productsByVendors);
 
         } catch (JAXBException exception) {
             exception.printStackTrace();
